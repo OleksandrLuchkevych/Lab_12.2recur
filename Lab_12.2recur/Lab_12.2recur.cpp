6702dfe7b715d5897ffdea61375efef261855c41
oleksandr.luchkevych.ri.2022@lpnu.ua <oleksandr.luchkevych.ri.2022@lpnu.ua>
2023-03-31 00:40:50 +0300

    Add project files.

---
+++ Lab_12.2recur/Lab_12.2recur.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+
+using namespace std;
+
+typedef int Info;
+struct Elem
+{
+    Elem* link;
+    Info info;
+};
+
+void Input(Elem*& first, Elem*& last, Info value)
+{
+    if (first == NULL)
+    {
+        first = new Elem;
+        first->info = value;
+        first->link = NULL;
+        last = first;
+    }
+    else
+    {
+        Input(first->link, last, value);
+    }
+}
+
+void Delete(Elem*& first, Elem*& last, Info& value)
+{
+    if (first == NULL) 
+    {
+        // Список порожній, повертаємо невизначене значення
+        value = -1;
+        return;
+    }
+    if (first == last) 
+    {
+        // Список містить один елемент, видаляємо його
+        value = first->info;
+        delete first;
+        first = NULL;
+        last = NULL;
+        return;
+    }
+    // Рекурсивно викликаємо функцію для наступного елемента
+    Delete(first->link, last, value);
+    if (first == NULL) {
+        // Якщо видалено перший елемент, оновлюємо значення змінної first
+        return;
+    }
+    if (first->link == NULL) 
+    {
+        // Якщо видалено останній елемент, оновлюємо значення змінної last
+        last = first;
+    }
+}
+
+bool Сheck(Elem* elem)
+{
+    if (elem->link == NULL)
+    {
+        return false;
+    }
+    if (elem->info > elem->link->info)
+    {
+        return true;
+    }
+    return Сheck(elem->link);
+}
+
+int main()
+{
+    Elem* first = NULL, * last = NULL;
+    int n = 12;
+    for (int i = 0; i < n; i++)
+    {
+        Input(first, last, i);
+    }
+    bool count = Сheck(first);
+    if (count == true)
+    {
+        cout << "List elements are sorted in non-descending order" << endl;
+    }
+    else
+    {
+        cout << "List elements are sorted in descending order" << endl;
+    }
+
+    cout << endl;
+
+    Info value;
+    for (int i = 0; i < n; i++)
+    {
+        Delete(first, last, value);
+        cout << value << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_12.2recur/Lab_12.2recur.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+            Elem* first = NULL, * last = NULL;
+            for (int i = 0; i < 5; i++) {
+                if (first == NULL)
+                {
+                    first = new Elem;
+                    first->info = i;
+                    first->link = NULL;
+                    last = first;
+                }
+                else
+                {
+                    Input(first->link, last, i);
+                }
+                bool t = Сheck(first);
+                Assert::AreEqual(t, false);
+            }
+		}
+	};
+}
